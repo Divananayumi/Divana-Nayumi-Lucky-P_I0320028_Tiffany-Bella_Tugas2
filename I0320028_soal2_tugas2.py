f772ffd4adf6f3ce73432857bf41769e1c438097
Divana Nayumi Lucky Pratama <divanayumi13@gmail.com>
2021-03-12 00:19:55 +0700

    Pengerjaan soal

---
+++ I0320028_soal2_tugas2.py
@@ -0,0 +1,42 @@
+#memasukkan data diri
+nama_lengkap = "Divana Nayumi Lucky Pratama"
+nama_panggilan = "Divana"
+nim = "I0320028"
+kelas = "A"
+tempat_lahir = "Surakarta"
+tanggal_lahir = int(20/2+3)
+bulan_lahir = "Agustus"
+tahun_lahir = int(40*50+2)
+umur = int(3*6)
+berat_badan = int(100/2-3)
+tinggi_badan = int(3*50+6)
+jenis_kelamin = "perempuan"
+agama = "Islam"
+print("Halo semua, perkenalkan nama saya", str(nama_lengkap)+",", "biasa dipanggil", str(nama_panggilan)+".")
+print("NIM saya", nim, "dari kelas", str(kelas)+".")
+print("Saya lahir di", tempat_lahir, "pada tanggal", tanggal_lahir, bulan_lahir, str(tahun_lahir)+".")
+print("Saat ini saya berusia", umur, str("tahun")+".")
+print("Saya memiliki berat badan", berat_badan, "kg dengan tinggi", tinggi_badan, str("cm")+".")
+print("Saya berjenis kelamin", jenis_kelamin, "dan beragama", agama)
+
+#memasukkan data pendidikan saat ini
+program_studi = "Teknik Industri"
+angkatan = int(25*81-5)
+perguruan_tinggi = "Universitas Sebelas Maret"
+print("Saya seorang mahasiswa jurusan", program_studi, "angkatan", angkatan, "di", str(perguruan_tinggi)+".")
+
+#memasukkan data alamat rumah
+kelurahan = "Kepatihan Wetan"
+R_T = int(10/2-2)
+R_W = int(2*2-3)
+kecamatan = "Jebres"
+kota = "Surakarta"
+print("Alamat rumah saya di", kelurahan, "RT", R_T, "RW", R_W, str(kecamatan)+",", str(kota)+".")
+
+#masukkan data tambahan
+hobi = "berenang dan membaca novel"
+ukuran_sepatu = int(5*7+5)
+ukuran_baju = "S atau M"
+print("Saya memiliki hobi", str(hobi)+".")
+print("Saya lebih nyaman menggunakan sepatu model sneakers berukuran", str(ukuran_sepatu)+".")
+print("Untuk baju, saya lebih suka menggunakan kaos berukuran", str(ukuran_baju)+".")
